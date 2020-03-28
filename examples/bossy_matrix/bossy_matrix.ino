bdd29c68de5177f874fb4b72c76e554a4cfae023
Mithi <mithi.sevilla@gmail.com>
2020-03-28 14:43:15 +0800

    Use uint8_t instead of int for some variables

--- examples/bossy_matrix/bossy_matrix.ino
+++ examples/bossy_matrix/bossy_matrix.ino
@@ -44,27 +44,27 @@ void setup() {
 
 void loop() {
 
-  for (int i = 0; i < NUMBER_OF_BUTTONS; i++) {
+  for (uint8_t i = 0; i < NUMBER_OF_BUTTONS; i++) {
     if (bossy.hasChangedState(buttons[i].id)) {
       update_button_display(buttons[i]);
     }
   }
-  
-  for (int i = 0; i < NUMBER_OF_SWITCHES; i++) {
+
+  for (uint8_t i = 0; i < NUMBER_OF_SWITCHES; i++) {
     if (bossy.hasChangedState(switches[i].id)) {
       update_switch_display(switches[i]);
     }
   }
 
-  for (int i = 0; i < NUMBER_OF_STICKS; i++) {
+  for (uint8_t i = 0; i < NUMBER_OF_STICKS; i++) {
     if (bossy.hasChangedState(sticks[i].id)) {
       if ( mode == STICKMODE) {
         update_stick_display(sticks[i]);
       }
     }
   }
-  
-  for (int i = 0; i < NUMBER_OF_POTENTIOMETERS; i++) {
+
+  for (uint8_t i = 0; i < NUMBER_OF_POTENTIOMETERS; i++) {
     if (bossy.hasChangedState(potentiometers[i])) {
       if (mode == POTMODE) {
         update_pot_display(i, potentiometers[i]);
@@ -72,58 +72,58 @@ void loop() {
     }
   }
 
-  toggle_mode_if_commanded();    
+  toggle_mode_if_commanded();
   matrix.writeDisplay();
 }
 
 void update_button_display(const struct Button button) {
-  uint8_t state = bossy.savedState(button.id);
-  uint8_t color = button_colors[state];
-  uint8_t x = button.pixel[Xi];
-  uint8_t y = button.pixel[Yi];
+  const uint8_t state = bossy.savedState(button.id);
+  const uint8_t color = button_colors[state];
+  const uint8_t x = button.pixel[Xi];
+  const uint8_t y = button.pixel[Yi];
   matrix.drawPixel(x, y, color);
-  
+
   matrix.writeDisplay();
 }
 
 void update_stick_display(const struct Stick stick) {
-  uint8_t state = bossy.savedState(stick.id);
-  
-  uint8_t color1 = stick_first_colors[state];
-  uint8_t color2 = stick_second_colors[state];
+  const uint8_t state = bossy.savedState(stick.id);
 
-  uint8_t x1 = stick.first_pixel[Xi];
-  uint8_t y1 = stick.first_pixel[Yi];     
+  const uint8_t color1 = stick_first_colors[state];
+  const uint8_t color2 = stick_second_colors[state];
 
-  uint8_t x2 = stick.second_pixel[Xi];
-  uint8_t y2 = stick.second_pixel[Yi];
-  
+  const uint8_t x1 = stick.first_pixel[Xi];
+  const uint8_t y1 = stick.first_pixel[Yi];
+
+  const uint8_t x2 = stick.second_pixel[Xi];
+  const uint8_t y2 = stick.second_pixel[Yi];
+
   matrix.drawPixel(x1, y1, color1);
   matrix.drawPixel(x2, y2, color2);
-  
+
   matrix.writeDisplay();
 }
 
 void update_switch_display(const struct SwitchSPDT sw) {
-      
-  uint8_t state = bossy.savedState(sw.id);
+  const uint8_t state = bossy.savedState(sw.id);
 
-  uint8_t color[3] = {sw_first_colors[state], sw_second_colors[state], sw_third_colors[state]};
-  uint8_t x[3] = {sw.first_pixel[Xi], sw.second_pixel[Xi], sw.third_pixel[Xi]};
-  uint8_t y[3] = {sw.first_pixel[Yi], sw.second_pixel[Yi], sw.third_pixel[Yi]};
+  const uint8_t color[3] = {sw_first_colors[state], sw_second_colors[state], sw_third_colors[state]};
+  const uint8_t x[3] = {sw.first_pixel[Xi], sw.second_pixel[Xi], sw.third_pixel[Xi]};
+  const uint8_t y[3] = {sw.first_pixel[Yi], sw.second_pixel[Yi], sw.third_pixel[Yi]};
 
-  for (int i = 0; i < 3; i++) {      
+  for (uint8_t i = 0; i < 3; i++) {
     matrix.drawPixel(x[i], y[i], color[i]);
   }
-  
+
   matrix.writeDisplay();
 }
 
 void update_pot_display(uint8_t pot_id, uint8_t input_id) {
-  uint16_t reading = bossy.savedReading(input_id);
-  uint16_t blocks = min((reading - 100) / 100, 8);  
-  
-  for (int i = 0; i < 8; i++) {      
+  const uint16_t reading = bossy.savedReading(input_id);
+  const uint16_t blocks = min((reading - 100) / 100, 8);
+  const uint8_t NUMBER_OF_BLOCKS = 8;
+
+  for (uint8_t i = 0; i < NUMBER_OF_BLOCKS; i++) {
     matrix.drawPixel(i, pot_id + 1, LED_GREEN);
   }
 
@@ -132,7 +132,7 @@ void update_pot_display(uint8_t pot_id, uint8_t input_id) {
     return;
   }
 
-  for (int i = 0; i <= blocks; i++) {    
+  for (uint8_t i = 0; i <= blocks; i++) {
     matrix.drawPixel(i, pot_id + 1, LED_RED);
   }
 
@@ -155,52 +155,58 @@ void display_begin() {
   for (int i = 0; i < NUMBER_OF_BUTTONS; i++) {
     update_button_display(buttons[i]);
   }
-  
+
   for (int i = 0; i < NUMBER_OF_SWITCHES; i++) {
     update_switch_display(switches[i]);
   }
-  
+
   matrix.writeDisplay();
 }
 
 void toggle_mode_if_commanded() {
 
-  bool is_stick_mode = bossy.savedState(LEFT_SWITCH_UPPER) == LEFT;
+  const bool is_stick_mode = bossy.savedState(LEFT_SWITCH_UPPER) == LEFT;
   bool toggled = false;
-  
-  if (is_stick_mode && mode !=STICKMODE) {
+
+  if (is_stick_mode && mode != STICKMODE) {
     toggled = true;
     mode = STICKMODE;
   }
 
-  if (!is_stick_mode && mode ==STICKMODE) {
+  if (!is_stick_mode && mode == STICKMODE) {
     toggled = true;
     mode = POTMODE;
   }
 
   if (toggled) {
-    
+
     matrix.setTextWrap(false);
     matrix.setTextSize(1);
     matrix.setTextColor(LED_RED);
-    matrix.clear();
-    matrix.setCursor(1, 0);
-    mode == STICKMODE ? matrix.print("1") : matrix.print("2");      
-    matrix.writeDisplay();
-    delay(2000);
-    
+
+    for (uint8_t i = 0; i < 4; i++) {
+      matrix.clear();
+      matrix.setCursor(1, 0);
+      mode == STICKMODE ? matrix.print("1") : matrix.print("2");
+      matrix.writeDisplay();
+      delay(100);
+      matrix.clear();
+      matrix.writeDisplay();
+      delay(100);
+    }
+
     matrix.clear();
     display_begin();
   }
 
-  if (mode==POTMODE) {
-    for (int i = 1; i < NUMBER_OF_POTENTIOMETERS; i++) {
+  if (mode == POTMODE) {
+    for (uint8_t i = 1; i < NUMBER_OF_POTENTIOMETERS; i++) {
       update_pot_display(i, potentiometers[i]);
     }
   }
 
-  if (mode==STICKMODE) {
-    for (int i = 0; i < NUMBER_OF_STICKS; i++) {
+  if (mode == STICKMODE) {
+    for (uint8_t i = 0; i < NUMBER_OF_STICKS; i++) {
         update_stick_display(sticks[i]);
     }
   }
